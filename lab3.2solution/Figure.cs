3b3c26824946691043f3c6adac81f24315c90010
Mark <Mark@WIN-K2879RTSAC4>
2023-12-03 18:38:07 +0200

    Добавьте файлы проекта.

---
+++ lab3.2solution/Figure.cs
@@ -0,0 +1,40 @@
+﻿class Figure
+{
+    private Point[] points;
+
+    public Figure(Point point1, Point point2, Point point3)
+    {
+        points = new Point[] { point1, point2, point3 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4)
+    {
+        points = new Point[] { point1, point2, point3, point4 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+    {
+        points = new Point[] { point1, point2, point3, point4, point5 };
+    }
+
+    private double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        int numPoints = points.Length;
+
+        for (int i = 0; i < numPoints; i++)
+        {
+            Point currentPoint = points[i];
+            Point nextPoint = points[(i + 1) % numPoints]; // Circular iteration to connect the last and first points
+
+            perimeter += GetSideLength(currentPoint, nextPoint);
+        }
+
+        Console.WriteLine($"Figure Perimeter: {perimeter}");
+    }
+}
---
+++ lab3.2solution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Create points for the figure
+        Point point1 = new Point(0, 0, "A");
+        Point point2 = new Point(0, 3, "B");
+        Point point3 = new Point(4, 0, "C");
+
+        // Create an instance of the Figure class with 3 points
+        Figure triangle = new Figure(point1, point2, point3);
+
+        // Calculate and display the perimeter of the figure
+        triangle.CalculatePerimeter();
+
+        // Keep the console window open
+        Console.ReadLine();
+    }
+}
